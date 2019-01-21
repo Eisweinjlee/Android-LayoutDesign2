bfe7de83afeaf0f1ac08ebc380132dd81c1469bc
Eisweinjlee <406050086@qq.com>
2019-01-21 22:02:28 +0900

    use ArrayAdapter<String> to start numbers catagory display

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,14 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -24,5 +32,61 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        //Create an arraylist of words
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+
+        /** Method A: Array Adapter + ListView
+         Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
+         adapter knows how to create layouts for each item in the list, using the
+         simple_list_item_1.xml layout resource defined in the Android framework.
+         This list item layout contains a single {@link TextView}, which the adapter will set to
+         display a single word.
+         */
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(itemsAdapter);
+
+        // Method B: Child TextViews add to root view in loop
+        // Find the root view so we can add child views
+//        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+//
+//        //looping to the end of the list
+//        int index = 0;
+//        while(index<words.size()){
+//            //Create the child views
+//            TextView wordView = new TextView(this);
+//            wordView.setText(words.get(index));
+//            rootView.addView(wordView);
+//            index ++;
+//        }
+
+
+        // Verify the contents
+//        Log.v("NumbersActivity", "Word at index 0: " + words.get(0));
+//        Log.v("NumbersActivity", "Word at index 1: " + words.get(1));
+//        Log.v("NumbersActivity", "Word at index 2: " + words.get(2));
+//        Log.v("NumbersActivity", "Word at index 3: " + words.get(3));
+//        Log.v("NumbersActivity", "Word at index 4: " + words.get(4));
+//        Log.v("NumbersActivity", "Word at index 5: " + words.get(5));
+//        Log.v("NumbersActivity", "Word at index 6: " + words.get(6));
+//        Log.v("NumbersActivity", "Word at index 7: " + words.get(7));
+//        Log.v("NumbersActivity", "Word at index 8: " + words.get(8));
+//        Log.v("NumbersActivity", "Word at index 9: " + words.get(9));
+
     }
 }
